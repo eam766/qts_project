f9c00ad2ded60e7baa32e7c9f795ddebdb245ae1
Émilie <202034805@collegeahuntsic.qc.ca>
2025-02-22 20:11:54 -0500

    carousel et modif page jeu

--- resources/js/Components/Acceuil/ListeJeux.jsx
+++ resources/js/Components/Acceuil/ListeJeux.jsx
@@ -1,54 +1,40 @@
 import "./ListeJeux.css";
-
-import { useState } from "react";
-
-import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos";
-import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
+import { Carousel } from "primereact/carousel";
+import "primereact/resources/themes/lara-light-indigo/theme.css";
+import "primereact/resources/primereact.min.css";
+import "primeicons/primeicons.css";
 import { Link } from "@inertiajs/react";
 
 export default function ListeJeux({ couvertures, visibleCount = 5 }) {
-    const [index, setIndex] = useState(0);
-
-    function toTheLeft() {
-        setIndex((prevIndex) => (prevIndex + 1) % couvertures.length);
-    }
-
-    function toTheRight() {
-        setIndex(
-            (prevIndex) =>
-                (prevIndex - 1 + couvertures.length) % couvertures.length
+    const itemTemplate = (couverture) => {
+        return (
+            <div
+                style={{
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                }}
+            >
+                <Link href={`/jeux/${couverture.id}`}>
+                    <img
+                        src={`https://images.igdb.com/igdb/image/upload/t_cover_big/${couverture.cover.image_id}.jpg`}
+                        alt="Jeu"
+                        className="jeux-cover"
+                    />
+                </Link>
+            </div>
         );
-    }
-
-    let imagesAffichees = couvertures.slice(index, index + visibleCount);
-
-    if (imagesAffichees.length < visibleCount) {
-        imagesAffichees = imagesAffichees.concat(
-            couvertures.slice(0, visibleCount - imagesAffichees.length)
-        );
-    }
+    };
 
     return (
         <div className="liste-jeux">
-            <button onClick={toTheLeft}>
-                <ArrowBackIosIcon />
-            </button>
-            <div className="jeux-container">
-                {imagesAffichees.map((couverture, i) => (
-                    <Link href={`/jeux/${couverture.id}`}>
-                        <img
-                            key={i}
-                            src={`https://images.igdb.com/igdb/image/upload/t_cover_big/${couverture.cover.image_id}.jpg`}
-                            alt="image"
-                            className="jeux-cover"
-                        />
-                    </Link>
-                ))}
-            </div>
-
-            <button onClick={toTheRight}>
-                <ArrowForwardIosIcon />
-            </button>
+            <Carousel
+                value={couvertures}
+                itemTemplate={itemTemplate}
+                numVisible={visibleCount}
+                numScroll={visibleCount}
+                circular
+            />
         </div>
     );
 }
