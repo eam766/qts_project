e3b1005aec5ba0930c0bc10255f9d9eae83d3f9e
VachonAlexandre <201367503@collegeahuntsic.qc.ca>
2025-03-09 17:53:23 -0400

    correction d'un bug de redirection apres avoir changer le mot de passe

--- app/Http/Controllers/Auth/NewPasswordController.php
+++ app/Http/Controllers/Auth/NewPasswordController.php
@@ -59,7 +59,7 @@ class NewPasswordController extends Controller
         // the application's home authenticated view. If there is an error we can
         // redirect them back to where they came from with their error message.
         if ($status == Password::PASSWORD_RESET) {
-            return redirect()->route('connexion')->with('status', __($status));
+            return redirect('/connexion')->with('status', __($status));
         }
 
         throw ValidationException::withMessages([
--- resources/js/Pages/Auth/ResetPassword.jsx
+++ resources/js/Pages/Auth/ResetPassword.jsx
@@ -17,7 +17,7 @@ export default function ResetPassword({ token, email }) {
     const submit = (e) => {
         e.preventDefault();
 
-        post(route("password.store"), {
+        post("/reset-password", {
             onFinish: () => reset("password", "password_confirmation"),
         });
     };
