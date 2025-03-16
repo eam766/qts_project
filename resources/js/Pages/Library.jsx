18b8583d21007f1780900316bf5f20aea169c6f0
Émilie <202034805@collegeahuntsic.qc.ca>
2025-03-16 16:28:11 -0400

    biblio design

--- resources/js/Pages/Library.jsx
+++ resources/js/Pages/Library.jsx
@@ -5,33 +5,111 @@ import logo from "@/assets/img/FrogLogo.png";
 
 export default function Library() {
     const images = [
-        { image: logo, name: "logo 1" },
-        { image: logo, name: "logo 2" },
-        { image: logo, name: "logo 3" },
-        { image: logo, name: "logo 4" },
-        { image: logo, name: "logo 5" },
-        { image: logo, name: "logo 6" },
-        { image: logo, name: "logo 7" },
-        { image: logo, name: "logo 8" },
-        { image: logo, name: "logo 9" },
-        { image: logo, name: "logo 10" },
-        { image: logo, name: "logo 11" },
-        { image: logo, name: "logo 12" },
-        { image: logo, name: "logo 13" },
+        {
+            image: logo,
+            name: "logo 1",
+            genres: ["Strategy", "Tactical", "Adventure"],
+            themes: ["Action", "Fantasy"],
+        },
+        {
+            image: logo,
+            name: "logo 2",
+            genres: ["Strategy", "Tactical", "Adventure"],
+            themes: ["Action", "Fantasy"],
+        },
+        {
+            image: logo,
+            name: "logo 3",
+            genres: ["Strategy", "Tactical", "Adventure"],
+            themes: ["Action", "Fantasy"],
+        },
+        {
+            image: logo,
+            name: "logo 4",
+            genres: ["Strategy", "Tactical", "Adventure"],
+            themes: ["Action", "Fantasy"],
+        },
+        {
+            image: logo,
+            name: "logo 5",
+            genres: ["Strategy", "Tactical", "Adventure"],
+            themes: ["Action", "Fantasy"],
+        },
+        {
+            image: logo,
+            name: "logo 6",
+            genres: ["Strategy", "Tactical", "Adventure"],
+            themes: ["Action", "Fantasy"],
+        },
+        {
+            image: logo,
+            name: "logo 7",
+            genres: ["Strategy", "Tactical", "Adventure"],
+            themes: ["Action", "Fantasy"],
+        },
+        {
+            image: logo,
+            name: "logo 8",
+            genres: ["Strategy", "Tactical", "Adventure"],
+            themes: ["Action", "Fantasy"],
+        },
+        {
+            image: logo,
+            name: "logo 9",
+            genres: ["Strategy", "Tactical", "Adventure"],
+            themes: ["Action", "Fantasy"],
+        },
+        {
+            image: logo,
+            name: "logo 10",
+            genres: ["Strategy", "Tactical", "Adventure"],
+            themes: ["Action", "Fantasy"],
+        },
+        {
+            image: logo,
+            name: "logo 11",
+            genres: ["Strategy", "Tactical", "Adventure"],
+            themes: ["Action", "Fantasy"],
+        },
+        {
+            image: logo,
+            name: "logo 12",
+            genres: ["Strategy", "Tactical", "Adventure"],
+            themes: ["Action", "Fantasy"],
+        },
+        {
+            image: logo,
+            name: "logo 13",
+            genres: ["Strategy", "Tactical", "Adventure"],
+            themes: ["Action", "Fantasy"],
+        },
     ];
 
     const itemTemplate = (item) => {
         return (
-            <div className="flex flex-col">
-                <div className="flex flex-row bg-gray-700">
+            <div className="flex flex-col m-2">
+                <div className="flex flex-row justify-center items-center">
                     <img
                         className="w-32 h-32"
                         src={item.image}
                         alt={item.name}
                     />
 
-                    <span className="font-bold">{item.name}</span>
-                    <button className="buttonRight ml-auto">Installer</button>
+                    <div className="ml-4 flex flex-col">
+                        <span className="AudioWideBlue">{item.name}</span>
+                        <span className="text-sm text-white">
+                            <span className="text-[#02d7f2]">Genres:</span>{" "}
+                            {item.genres.join(", ")}
+                        </span>
+                        <span className="text-sm text-white">
+                            <span className="text-[#02d7f2]">Thèmes:</span>{" "}
+                            {item.themes.join(", ")}
+                        </span>
+                    </div>
+
+                    <button className="buttonRight AudioWideBlue ml-auto">
+                        Installer
+                    </button>
                 </div>
             </div>
         );
@@ -53,9 +131,9 @@ export default function Library() {
                             <OrderList
                                 value={images}
                                 itemTemplate={itemTemplate}
-                                header="Products"
                                 filter
                                 filterBy="name"
+                                className="custom-orderlist"
                             ></OrderList>
                         </div>
                     </div>
